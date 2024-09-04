7378b253408bc3b3f30418839de2e829dac3dd5d
Craig McKeachie <craigdmckeachie@gmail.com>
2024-09-04 15:08:56 -0400

    customer card design

--- src/customers/CustomerCard.tsx
+++ src/customers/CustomerCard.tsx
@@ -7,6 +7,10 @@ interface CustomerCardProps {
 function CustomerCard({ customer }: CustomerCardProps) {
   return (
     <div className="card p-4">
+      <span className="customer-circle d-flex bg-secondary fs-3 text-white align-items-center justify-content-center rounded-circle">
+        {customer.firstName?.substring(0, 1)}
+        {customer.lastName?.substring(0, 1)}
+      </span>
       <div className="card-body">
         <h5 className="card-title">
           {customer.firstName} {customer.lastName}
