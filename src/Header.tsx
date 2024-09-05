52ddb08353b48d1eb355f1198333de5af0925329
Craig McKeachie <craigdmckeachie@gmail.com>
2024-09-05 10:36:13 -0400

    sidebar padding

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="container-fluid d-flex justify-content-between py-4 bg-body-tertiary border-bottom">
+    <header className="container-fluid d-flex justify-content-between py-4 px-4 bg-body-tertiary border-bottom">
       <section className="d-flex gap-2 align-items-center">
         <Link to="/">
           <img src="/logoipsum-245.svg" alt="logo" />
